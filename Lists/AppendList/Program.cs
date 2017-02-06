f75cab605497c0de3fd70a49c7af7bae3e96adb9
TrifonEstov <don.tito.g@gmail.com>
2017-02-06 20:39:17 +0200

    New program added!

---
+++ Lists/AppendList/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AppendList
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class AppencList
+    {
+        public static void Main()
+        {
+            string[] input = Console.ReadLine().Split('|').ToArray();
+            List<int> numbers = new List<int>();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                numbers.AddRange(input[i].Split(new char[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+            }
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number + "" );
+            }
+        }
+    }
+}
