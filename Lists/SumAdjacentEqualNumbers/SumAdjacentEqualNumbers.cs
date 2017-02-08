20e406e8d0fa50efb569b998cbc9e79199fe21ca
TrifonEstov <don.tito.g@gmail.com>
2017-02-08 23:27:12 +0200

    New program added but not work properly - SumAdjacentEqualNumbers!!!

---
+++ Lists/SumAdjacentEqualNumbers/SumAdjacentEqualNumbers.cs
@@ -0,0 +1,26 @@
+﻿namespace SumAdjacentEqualNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SumAdjacentEqualNumbers
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var sum = 0;
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] == input[i + 1])
+                {
+                    sum += input[i] + input[i + 1];
+                }
+            }
+            foreach (var item in input)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
