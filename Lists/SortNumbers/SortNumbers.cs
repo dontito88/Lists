991444bb96b1edddca71355c68b2386bfad232c8
TrifonEstov <don.tito.g@gmail.com>
2017-02-09 00:03:18 +0200

    new program added!

---
+++ Lists/SortNumbers/SortNumbers.cs
@@ -0,0 +1,16 @@
+﻿namespace SortNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SortNumbers
+    {
+        public static void Main()
+        {
+            List<decimal> numbers = Console.ReadLine().Split(' ').Select(decimal.Parse).ToList();
+            numbers.Sort();
+            Console.WriteLine(string.Join(" <=", numbers));
+        }
+    }
+}
