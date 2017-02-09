d893ae82d1391ab02882782f904aa074b80b121b
TrifonEstov <don.tito.g@gmail.com>
2017-02-09 21:47:39 +0200

    All problems, except task No 7, are solved!

---
+++ Lists/SquareNumbers/SquareNumbers.cs
@@ -0,0 +1,24 @@
+﻿namespace SquareNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SquareNumbers
+    {
+        public static void Main()
+        {
+            List<double> numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+            numbers.Sort();
+            numbers.Reverse();
+
+            foreach (var number in numbers)
+            {
+                if (Math.Sqrt(number) == (int) Math.Sqrt(number))
+                {
+                    Console.Write(number + " ");
+                }
+            }
+        }
+    }
+}
