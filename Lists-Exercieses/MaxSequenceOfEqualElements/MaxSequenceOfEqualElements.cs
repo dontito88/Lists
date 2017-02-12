fcebb214b9340bc3b5c0a1609c28ae9adc0d7e6b
TrifonEstov <don.tito.g@gmail.com>
2017-02-12 13:27:14 +0200

    Exercise task upload!

---
+++ Lists-Exercieses/MaxSequenceOfEqualElements/MaxSequenceOfEqualElements.cs
@@ -0,0 +1,23 @@
+﻿namespace MaxSequenceOfEqualElements
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq; 
+
+    public class MaxSequenceOfEqualElements
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            for (int i = 0; i < input.Count; i++)
+            {
+                var counter = 0;
+                if (input[i] == input[i + 1])
+                {
+                    counter++;
+                }
+                Console.WriteLine(counter);
+            }
+        }
+    }
+}
