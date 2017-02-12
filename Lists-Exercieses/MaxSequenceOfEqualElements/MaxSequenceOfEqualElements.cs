001705d7ce1aed2f5a3add549b3617f68b6ef81f
TrifonEstov <don.tito.g@gmail.com>
2017-02-12 14:54:27 +0200

    New program added!

--- Lists-Exercieses/MaxSequenceOfEqualElements/MaxSequenceOfEqualElements.cs
+++ Lists-Exercieses/MaxSequenceOfEqualElements/MaxSequenceOfEqualElements.cs
@@ -8,16 +8,48 @@
     {
         public static void Main()
         {
-            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
-            for (int i = 0; i < input.Count; i++)
+            string[] inputNumbers = Console.ReadLine().Split();
+            int[] parsedNumbers = new int[inputNumbers.Length];
+
+            for (int i = 0; i < inputNumbers.Length; i++)
             {
-                var counter = 0;
-                if (input[i] == input[i + 1])
-                {
-                    counter++;
-                }
-                Console.WriteLine(counter);
+                parsedNumbers[i] = int.Parse(inputNumbers[i]);
             }
+
+            List<int> longestSubsequence = new List<int>();
+            List<int> currentSubsequence = new List<int>();
+
+            currentSubsequence.Add(parsedNumbers[0]);
+
+            for (int i = 1; i < parsedNumbers.Length; i++)
+            {
+                if (parsedNumbers[i] == currentSubsequence[0])
+                {
+                    currentSubsequence.Add(parsedNumbers[i]);
+                }
+                else
+                {
+                    if (currentSubsequence.Count > longestSubsequence.Count)
+                    {
+                        longestSubsequence = new List<int>();
+                        for (int j = 0; j < currentSubsequence.Count; j++)
+                        {
+                            longestSubsequence.Add(currentSubsequence[j]);
+                        }
+                    }
+                    currentSubsequence = new List<int>();
+                    currentSubsequence.Add(parsedNumbers[i]);
+                }
+            }
+            if (currentSubsequence.Count > longestSubsequence.Count)
+            {
+                longestSubsequence = new List<int>();
+                for (int j = 0; j < currentSubsequence.Count; j++)
+                {
+                    longestSubsequence.Add(currentSubsequence[j]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", longestSubsequence));
         }
     }
 }
---
+++ Lists-Exercieses/SumReversedNumbers/SumReversedNumbers.cs
@@ -0,0 +1,25 @@
+﻿namespace SumReversedNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SumReversedNumbers
+    {
+        public static void Main()
+        {
+            string[] inputNumbers = Console.ReadLine().Split();
+            int sum = 0;
+
+            for (int i = 0; i < inputNumbers.Length; i++)
+            {
+                string currentNumber = inputNumbers[i];
+                string reversedNumber = string.Join("", currentNumber.Reverse());
+
+                int parsedNumber = int.Parse(reversedNumber);
+                sum += parsedNumber;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
